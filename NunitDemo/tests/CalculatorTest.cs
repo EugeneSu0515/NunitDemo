c9c2eaef55675481a8ba87d384c0920b4ae9f565
EugeneSu <beckhamnaing72@gmail.com>
2018-08-08 23:19:37 +0800

    Add First Test Case
    1)Add Calculator class and Add function
    2)Add CalculatorTest Test Class
    3)Add Test Function for test Calculator.Add function

---
+++ NunitDemo/tests/CalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NunitDemo.source;
+using NUnit.Framework;
+
+namespace NunitDemo.tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void Add_Input_First_1_Second_2_Return_3()
+        {
+            //arrange
+            var target =  new Calculator();
+            var firstNumber = 1;
+            var secondNumber = 2;
+            var expected = 3;
+            //act
+            var actual = target.Add(firstNumber, secondNumber);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
